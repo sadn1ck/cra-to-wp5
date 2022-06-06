75b7884f6a4f59dcb2045bd7f8733292302f7942
Anik Das <anikdas0811@gmail.com>
2022-06-06 12:18:44 +0530

    feat: bundle splitting in prod
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
@@ -10,4 +11,31 @@ module.exports = {
       ],
     }),
   ],
+  output: {
+    path: path.resolve(__dirname, "../build"),
+    filename: "[name].[contenthash].js",
+  },
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      chunks: "all",
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // src: https://medium.com/hackernoon/the-100-correct-way-to-split-your-chunks-with-webpack-f8a9df5b7758
+            console.log(module.context);
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `${packageName.replace("@", "")}`;
+          },
+        },
+      },
+    },
+  },
 };
